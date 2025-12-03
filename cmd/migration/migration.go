d4724d991eecc06d8f66995ae8b9b5a87f9bc9de
Bhupendra <bhupendra@Bhupendras-MacBook-Pro.local>
2025-12-03 14:37:07 +0530

    feat: added seed and migration

--- cmd/migration/migration.go
+++ cmd/migration/migration.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
 
+	"github.com/bhupendra-dudhwal/kart-challenge/internal/builder"
+	"github.com/bhupendra-dudhwal/kart-challenge/internal/core/services/migration"
+	"go.uber.org/zap"
+)
+
+func main() {
+	var envPath string
+	flag.StringVar(&envPath, "env", "config/config.yaml", "Path to environment config file")
+	flag.Parse()
+
+	ctx := context.Background()
+	appBuilder := builder.NewAppBuilder(ctx)
+
+	if err := appBuilder.LoadConfig(envPath); err != nil {
+		log.Fatalf("failed to load config (%s): %+v", envPath, err)
+	}
+
+	logger, err := appBuilder.SetLogger()
+	if err != nil {
+		log.Fatalf("failed to initialize logger: %+v", err)
+	}
+	defer logger.Close()
+
+	if err := appBuilder.SetDatabaseRepository(); err != nil {
+		logger.Error("failed to initialize database", zap.Error(err))
+		os.Exit(1)
+	}
+
+	dbClient := appBuilder.GetDbClient()
+	config := appBuilder.GetConfig()
+
+	migrationPorts := migration.NewMigrationService(config, logger, dbClient)
+	migrationPorts.Migrate()
+	migrationPorts.Seed()
 }
